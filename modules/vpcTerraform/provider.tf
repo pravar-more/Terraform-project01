5783446bdb8f4ec667cfbca92a8f5f7f7bf01571
Pravar More <88570821+pravar-more@users.noreply.github.com>
2024-12-06 11:47:14 +0530

    Update provider.tf

--- modules/vpcTerraform/provider.tf
+++ modules/vpcTerraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    region = var.REGION
-    access_key = var.AWS_ACCESS_KEY 
-    secret_key = var.AWS_SECRET_KEY 
+  region     = var.REGION
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
 
 }
