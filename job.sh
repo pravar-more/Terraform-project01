48a330e218f0b0a1a3bff7a69832eb5dc9d73d2c
Pravar More <88570821+pravar-more@users.noreply.github.com>
2025-01-03 17:28:09 +0530

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,22 +1,24 @@
-
 #!/bin/bash
 
 ################
 ################
 #Define Variables
-AWS_ACCESS_KEY = ""
-AWS_SECRET_KEY = ""
-REGION = ""
-KEY_NAME = ""
+AWS_ACCESS_KEY = "AKIAQQABDQUAVYOYYA4Q"
+AWS_SECRET_KEY = "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"
+REGION = "us-east-1"
+KEY_NAME = "NVirginia_key_VBanner"
 JENKINS_MACHINE_IP = ""
 
-
+# Export AWS keys as environment variables 
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY 
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
+export REGION=$REGION
 
 #install git
 sudo yum install git -y
 
 #clone repo
-git clone https://github.com/pravar-more/AWSCICD-Terraform.git
+#git clone https://github.com/pravar-more/AWSCICD-Terraform.git
 
 #git remote
 git remote add origin https://github.com/pravar-more/AWSCICD-Terraform.git
@@ -63,15 +65,26 @@ echo "Setup complete!"
 
 ######################################################
 ######################################################
+#AWS Configure
+aws configure
+$AWS_ACCESS_KEY
+$AWS_SECRET_KEY
+$REGION
+table
+
+
 #Provisioning Terraform Resources
-cd /modules/jenkinsEC2
+cd ./AWSCICD-Terraform/modules/jenkinsEC2
 terraform init
 terraform validate
 terraform fmt
-terraform plan
-terraform apply -var 'AWS_ACCESS_KEY=${AWS_ACCESS_KEY}' -var 'AWS_SECRET_KEY=${AWS_SECRET_KEY}' -var 'REGION=${REGION}'
-yes
-# Capture the output variable value 
-JENKINS_MACHINE_IP=$(terraform output -raw instance_public_ip) 
-# Use the captured value 
+
+# Plan the Terraform changes 
+echo "Planning Terraform changes..." 
+terraform plan -var "AWS_ACCESS_KEY=$AWS_ACCESS_KEY" -var "AWS_SECRET_KEY=$AWS_SECRET_KEY" -var "REGION=$REGION" -out=tfplan
+terraform apply -auto-approve tfplan
+
+# Capture the output variable value
+JENKINS_MACHINE_IP=$(terraform output -raw instance_public_ip)
+# Use the captured value
 echo "The IP address of the Jenkins instance is: $JENKINS_MACHINE_IP"
